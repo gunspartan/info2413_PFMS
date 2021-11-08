038a82ebf8aac34359fb74438bd986101b3f4180
Gunn Hwang <gunhwang99@gmail.com>
2021-11-07 17:58:36 -0800

    Remove MainFrame.java from .gitignore

---
+++ info2413_PFMS/MainFrame.java
@@ -0,0 +1,35 @@
+package info2413_PFMS;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+	
+	private ContainerPanel containerPanel;
+
+	
+	public MainFrame(String text) throws Exception {
+		super(text);
+		// Set layout manager
+		setLayout(new BorderLayout());
+		// Create Swing component
+		containerPanel = new ContainerPanel();
+		// Add Swing components to content
+		add(containerPanel);
+
+		
+	
+	}
+	
+//	public JPanel getLoginPanel() {
+//		return loginPanel;
+//	}
+//	
+//	public JPanel getRegisterPanel() {
+//		return registerPanel;
+//	}
+}
